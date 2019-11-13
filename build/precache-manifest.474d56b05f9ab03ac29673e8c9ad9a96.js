18757a396d8009f4d63131bd4c5a035fe57fef8a
AMER <amer95521@gmail.com>
2019-11-13 11:15:21 +0530

    Add npm run build

---
+++ build/precache-manifest.474d56b05f9ab03ac29673e8c9ad9a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d113febe770fd0a8348d3008713a850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f424de3b6a03772a4a0",
+    "url": "/static/css/2.f2987ecf.chunk.css"
+  },
+  {
+    "revision": "59c524d3c4c69d576a04",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0f424de3b6a03772a4a0",
+    "url": "/static/js/2.03b0e33b.chunk.js"
+  },
+  {
+    "revision": "59c524d3c4c69d576a04",
+    "url": "/static/js/main.30e4fe4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
